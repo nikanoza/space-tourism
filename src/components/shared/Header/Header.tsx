494fa6142437eb3ae6d895a509bae43282b5cb85
hilu <nikanozadze1010@gmail.com>
2022-12-16 18:50:16 +0400

    style: make home page responsive

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,12 +1,76 @@
+import { NavLink, useLocation } from "react-router-dom";
 import { Logo, MenuIcon } from "svg";
-import Menu from "../Menu";
 import classes from "./header.module.scss";
 
 const Header: React.FC<{ setShowMenu: (bool: boolean) => void }> = (props) => {
+  const location = useLocation();
   return (
     <header className={classes.header}>
       <Logo />
-      <MenuIcon onClick={() => props.setShowMenu(true)} />
+      <div className={classes["menu-mb"]}>
+        <MenuIcon onClick={() => props.setShowMenu(true)} />
+      </div>
+      <div className={classes.line}></div>
+      <div className={classes["menu-ds"]}>
+        <ul className={classes.nav}>
+          <li>
+            <NavLink
+              to="/home"
+              className={({ isActive }) => (isActive ? classes.active : "")}
+            >
+              <span className={classes.numeric}>00</span>
+              home
+            </NavLink>
+            <div
+              className={
+                location.pathname.includes("home") ? classes.active : ""
+              }
+            ></div>
+          </li>
+          <li>
+            <NavLink
+              to="/destination"
+              className={({ isActive }) => (isActive ? classes.active : "")}
+            >
+              <span className={classes.numeric}>01</span>
+              destination
+            </NavLink>
+            <div
+              className={
+                location.pathname.includes("destination") ? classes.active : ""
+              }
+            ></div>
+          </li>
+          <li>
+            <NavLink
+              to="/crew"
+              className={({ isActive }) => (isActive ? classes.active : "")}
+            >
+              <span className={classes.numeric}>02</span>
+              crew
+            </NavLink>
+            <div
+              className={
+                location.pathname.includes("crew") ? classes.active : ""
+              }
+            ></div>
+          </li>
+          <li>
+            <NavLink
+              to="/technology"
+              className={({ isActive }) => (isActive ? classes.active : "")}
+            >
+              <span className={classes.numeric}>03</span>
+              technology
+            </NavLink>
+            <div
+              className={
+                location.pathname.includes("technology") ? classes.active : ""
+              }
+            ></div>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,14 +3,16 @@ import classes from "./home.module.scss";
 const Home = () => {
   return (
     <div className={classes.home}>
-      <h3 className={classes.describe}>SO, YOU WANT TO TRAVEL TO</h3>
-      <h1 className={classes.title}>space</h1>
-      <h4 className={classes.description}>
-        Let’s face it; if you want to go to space, you might as well genuinely
-        go to outer space and not hover kind of on the edge of it. Well sit
-        back, and relax because we’ll give you a truly out of this world
-        experience!
-      </h4>
+      <div className={classes.left}>
+        <h3 className={classes.describe}>SO, YOU WANT TO TRAVEL TO</h3>
+        <h1 className={classes.title}>space</h1>
+        <h4 className={classes.description}>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </h4>
+      </div>
       <button className={classes.explore}>EXPLORE</button>
     </div>
   );

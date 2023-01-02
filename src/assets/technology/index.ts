d88b438cdb737167a4bbb46dff965d5aee92da7d
hilu <nikanozadze1010@gmail.com>
2023-01-02 23:23:32 +0400

    feat: finish markup

--- src/assets/technology/index.ts
+++ src/assets/technology/index.ts
@@ -3,4 +3,4 @@ export { default as LaunchVehiclePortrait } from "./image-launch-vehicle-portrai
 export { default as SpaceCapsuleLandscape } from "./image-space-capsule-landscape.jpg";
 export { default as SpaceCapsulePortrait } from "./image-space-capsule-portrait.jpg";
 export { default as SpaceportLandscape } from "./image-spaceport-landscape.jpg";
-export { default as SpaceportPortrait } from "./image-spaceport-landscape.jpg";
+export { default as SpaceportPortrait } from "./image-spaceport-portrait.jpg";
